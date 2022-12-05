c2b29509197062d86bd666272187810be2365061
GspaceDevelopers <115160413+GspaceDevelopers@users.noreply.github.com>
2022-12-05 11:10:16 -0300

    Add files via upload

---
+++ src/schemas/Pedidos.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+
+const Pedidos = new Schema({
+    idpedido: String,
+    nomecliente: String,
+    uid: String,
+    cpf: Number,
+    produto: {
+        modelo: String,
+    },
+    endereco: String,
+    cep: String,
+    data: String,
+    hora: String,
+    status: String,
+    valor: String
+})
+export default model("Pedidos", Pedidos);
