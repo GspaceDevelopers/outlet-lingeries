1f10ff4b19bfe9c0f77db29ac4b238bc9dbc8f72
GspaceDevelopers <115160413+GspaceDevelopers@users.noreply.github.com>
2022-12-05 11:24:35 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class App {
     this.server = express();
 
     mongoose.connect(
-      "mongodb+srv://gspace:gspace@gspacer11.cbpzpqq.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://outlet-lingeries:outletLingeries@outlet-lingeries.ci7ugqj.mongodb.net/test",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
