1e875801f6a53f03671c5705eee3a39a37dab2b2
GspaceDevelopers <115160413+GspaceDevelopers@users.noreply.github.com>
2022-12-09 08:45:19 -0300

    Update Sac.js

--- src/schemas/Sac.js
+++ src/schemas/Sac.js
@@ -3,7 +3,8 @@ import { model, Schema } from 'mongoose'
 const Sac = new Schema({
     nome: String,
     email: String,
-    mensagem: String
+    mensagem: String,
+    data:String
 })
 
 export default new model('Sac', Sac)
