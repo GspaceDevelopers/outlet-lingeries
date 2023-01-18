d44ad5ada18ba9ae15aaca69ec3d456415e7c892
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-01-18 13:33:45 -0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -55,6 +55,7 @@ routes.get("/pedidos/cpf", PedidosController.get6);
 routes.get("/pedidos/valor", PedidosController.get7);
 routes.get("/data", PedidosController.get8);
 routes.get("/idLojafranqueada",PedidosController.get9);
+routes.get("/idLojafranqueada",PedidosController.get10);
 routes.delete("/pedidos/:_id", PedidosController.delete);
 routes.put("/pedidos/:idpedido", PedidosController.update);
 
